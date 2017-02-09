c7bf43ffed7b6fc595faaf9d96927a15d1bfeaac
Pedro Leandro Coutin Portuondo <pcoutin@ucsd.edu>
2017-02-09 11:30:07 -0800

    done

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -2,45 +2,52 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+  initializePage();
 })
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	console.log("Javascript connected!");
+  $(".friend a").click(changeName);
+  console.log("Javascript connected!");
+}
+
+function changeName(e) {
+  e.preventDefault();
+  $(e.target).text(anagrammedName(e.target.innerText));
+  /* console.log(e); */
 }
 
 function anagrammedName(name) {
-	// Thanks, Internet Anagram Server!
-	
-	if (name == "Doug Engelbart") {
-		return "Notable Grudge";
-	} 
-	else if (name == "Ivan Sutherland") {
-		return "Vandal Heist Run";
-	}
-	else if (name == "JCR Licklider") {
-		return "Crick Rid Jell";
-	}
-	else if (name == "Vannevar Bush") {
-		return "Ravens Van Hub";
-	}
-	else if (name == "Alan C. Kay") {
-		return "Canal Yak";
-	}
-	else if (name == "Allen Newell") {
-		return "Ellen All New";
-	}
-	else if (name == "Lucy Suchman") {
-		return "Lunacy Chums";
-	}
-	else if (name == "Grace Hopper") {
-		return "Gear Chopper";
-	}
-	else {
-		console.log(name + " not known for anagramming.");
-		return name;
-	}
+  // Thanks, Internet Anagram Server!
+
+  if (name == "Doug Engelbart") {
+    return "Notable Grudge";
+  } 
+  else if (name == "Ivan Sutherland") {
+    return "Vandal Heist Run";
+  }
+  else if (name == "JCR Licklider") {
+    return "Crick Rid Jell";
+  }
+  else if (name == "Vannevar Bush") {
+    return "Ravens Van Hub";
+  }
+  else if (name == "Alan C. Kay") {
+    return "Canal Yak";
+  }
+  else if (name == "Allen Newell") {
+    return "Ellen All New";
+  }
+  else if (name == "Lucy Suchman") {
+    return "Lunacy Chums";
+  }
+  else if (name == "Grace Hopper") {
+    return "Gear Chopper";
+  }
+  else {
+    console.log(name + " not known for anagramming.");
+    return name;
+  }
 }
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,14 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
-	// Your code goes here
- }
+  var newName = req.query.name;
+  var newDesc = req.query.description;
+  data.friends.push({
+    'name': newName,
+    'description': newDesc,
+    'imageURL': 'http://lorempixel.com/400/400/people'
+  });
+  res.render('index', {
+    'data': data
+  });
+};
